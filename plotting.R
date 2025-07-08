d8556f347aa60dbcb738fa4be3db17076853a189
donaghegan <donagh.egan@ucdconnect.ie>
2025-07-08 23:35:51 +0100

    plotting umap

--- plotting.R
+++ plotting.R
@@ -6,6 +6,7 @@ pdf("/home/degan/msk/figures/umap_sample_embedding.pdf", width = 6, height = 6)
 ggplot(df_plot, aes(x = UMAP1, y = UMAP2, color = CANCER_TYPE)) +
   geom_point(alpha = 0.7, size = 1) +
   labs(title = "UMAP of Sample Embeddings", color = "Cancer Type") +
-  theme_minimal(base_size = 10) 
+  theme_minimal(base_size = 10) +
+  theme(legend.position = "none")
 dev.off()
 
