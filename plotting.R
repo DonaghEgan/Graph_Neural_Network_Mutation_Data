bb8d0bbb60b021caa52dad41a80e3fbdded462cc
donaghegan <donagh.egan@ucdconnect.ie>
2025-07-08 23:34:59 +0100

    plotting umap

--- plotting.R
+++ plotting.R
@@ -2,12 +2,10 @@ library(ggplot2)
 
 df_plot <- read.csv("/home/degan/msk/data/umap_embedding_for_R.csv")
 
-pdf("/home/degan/msk/figures/umap_sample_embedding.pdf", width = 5, height = 5)
+pdf("/home/degan/msk/figures/umap_sample_embedding.pdf", width = 6, height = 6)
 ggplot(df_plot, aes(x = UMAP1, y = UMAP2, color = CANCER_TYPE)) +
-  geom_point(alpha = 0.7, size = 1.5) +
+  geom_point(alpha = 0.7, size = 1) +
   labs(title = "UMAP of Sample Embeddings", color = "Cancer Type") +
-  theme_minimal(base_size = 10) +
-  theme(legend.position = "top") +
-  guides(color = guide_legend(ncol = 3))
+  theme_minimal(base_size = 10) 
 dev.off()
 
